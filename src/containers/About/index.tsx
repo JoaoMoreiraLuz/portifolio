3d82c47cca8b9b57cb67bb2844dcda6f2d3c6e24
Joao Gustavo <joaogustavo9992@outlook.com>
2024-07-24 13:48:06 -0300

    creating the structure

---
+++ src/containers/About/index.tsx
@@ -0,0 +1,14 @@
+import Title from '../../components/Title'
+
+const About = () => (
+  <section>
+    <Title>Sobre mim:</Title>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia deserunt,
+      officiis, qui consequuntur atque obcaecati nostrum odio quibusdam aliquam
+      possimus quaerat soluta quod excepturi ea! Atque a autem optio quod?
+    </p>
+  </section>
+)
+
+export default About
---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Title from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/JoaoMoreiraLuz.png" alt="" />
+    <Title>João Gustavo</Title>
+  </aside>
+)
+
+export default Sidebar
