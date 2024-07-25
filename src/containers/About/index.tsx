5f27d3a16e766cabfc3ad00a3013630cdf25320e
Joao Gustavo <joaogustavo9992@outlook.com>
2024-07-25 15:13:41 -0300

    creating the 'about me' section

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,13 +1,19 @@
 import Title from '../../components/Title'
+import Paragraph from '../../components/Paragraph'
+import { GithubSection } from './styles'
 
 const About = () => (
   <section>
-    <Title>Sobre mim:</Title>
-    <p>
+    <Title fontSize={16}>Sobre mim:</Title>
+    <Paragraph type="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia deserunt,
       officiis, qui consequuntur atque obcaecati nostrum odio quibusdam aliquam
       possimus quaerat soluta quod excepturi ea! Atque a autem optio quod?
-    </p>
+    </Paragraph>
+    <GithubSection>
+      <img src="https://github-readme-stats.vercel.app/api?username=JoaoMoreiraLuz&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=JoaoMoreiraLuz&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSection>
   </section>
 )
 
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Title from '../../components/Title'
 const Sidebar = () => (
   <aside>
     <img src="https://github.com/JoaoMoreiraLuz.png" alt="" />
-    <Title>João Gustavo</Title>
+    <Title fontSize={20}>João Gustavo</Title>
   </aside>
 )
 
